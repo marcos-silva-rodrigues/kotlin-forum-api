890f619d0aea83b565f42f6fd1e3d940175a9478
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-04 14:45:10 -0300

    feat: init

---
+++ src/test/kotlin/com/rodrigues/silva/marcos/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
