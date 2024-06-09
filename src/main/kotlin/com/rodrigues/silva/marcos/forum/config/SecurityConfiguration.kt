56181965891bfbe518d6023d86f46807e71a2363
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-09 15:28:53 -0300

    feat: using filters

--- src/main/kotlin/com/rodrigues/silva/marcos/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/rodrigues/silva/marcos/forum/config/SecurityConfiguration.kt
@@ -1,7 +1,10 @@
 package com.rodrigues.silva.marcos.forum.config
 
+import com.rodrigues.silva.marcos.forum.security.JWTAuthenticationFilter
+import com.rodrigues.silva.marcos.forum.security.JwtLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -13,25 +16,30 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration{
-
+class SecurityConfiguration(
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JwtUtil
+) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
+            csrf { disable() }
             authorizeRequests {
+                authorize(HttpMethod.POST,"/login", permitAll)
                 authorize("/topicos", hasAuthority("LEITURA_ESCRITA"))
                 authorize("*", authenticated)
             }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtLoginFilter(authenticationManager = authenticationManager(), jwtUtil = jwtUtil))
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JWTAuthenticationFilter(jwtUtil))
             sessionManagement {
                 sessionCreationPolicy = SessionCreationPolicy.STATELESS
             }
-            formLogin {
-                disable()
-            }
             httpBasic {  }
+            formLogin { disable() }
         }
 
         return http.build()
@@ -39,16 +47,12 @@ class SecurityConfiguration{
 
     @Bean
     fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
-
     @Bean
-    fun authenticationManager(
-        userDetailsService: UserDetailsService,
-        passwordEncoder: PasswordEncoder): AuthenticationManager {
-        val authenticationProvider = DaoAuthenticationProvider()
-        authenticationProvider.setUserDetailsService(userDetailsService)
-        authenticationProvider.setPasswordEncoder(passwordEncoder)
-
-        return ProviderManager(authenticationProvider)
-    }
+    fun authenticationManager(): AuthenticationManager =
+        ProviderManager(DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService)
+                it.setPasswordEncoder(encoder())
+            })
 
 }
