2f40db6891a295dc7431677d61e57dd10a11fb3b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-08 21:23:52 -0300

    fix: correct package

--- src/main/kotlin/com/rodrigues/silva/marcos/forum/repository/TopicoRepository.kt
+++ src/main/kotlin/com/rodrigues/silva/marcos/forum/repository/TopicoRepository.kt
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.Query
 interface TopicoRepository: JpaRepository<Topico, Long> {
 
     fun findByCursoNome(nomeCurso: String, paginacao: Pageable): Page<Topico>
-    @Query("SELECT new br.com.alura.forum.dto.TopicoPorCategoriaDto(curso.categoria, count(t)) FROM Topico t JOIN t.curso curso GROUP BY curso.categoria")
+    @Query("SELECT new com.rodrigues.silva.marcos.forum.dto.TopicoPorCategoriaDto(curso.categoria, count(t)) FROM Topico t JOIN t.curso curso GROUP BY curso.categoria")
     fun relatorio(): List<TopicoPorCategoriaDto>
 }
